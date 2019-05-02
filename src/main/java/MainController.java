7a02cf04a71260d1842cf914f0232dec24e1931a
Trylockon <trylockon@gmail.com>
2019-05-02 19:27:35 +0300

    Initial commit

---
+++ src/main/java/MainController.java
@@ -0,0 +1,14 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("api")
+public class MainController {
+
+    @RequestMapping(value={"/","/index"},method = RequestMethod.GET)
+    public String index(Model model){
+        return  "index";
+    }
+}
